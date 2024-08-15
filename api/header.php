d33c311e84aef1f24ee6f77f9f8fffde4d4507bb
gelvaziosenac <gelvaziosenac2022@gmail.com>
2024-08-14 22:57:23 -0300

    ok

--- api/header.php
+++ api/header.php
@@ -2,28 +2,28 @@
 // echo '<link rel="stylesheet" href="style.css">';
 //echo '<link rel="stylesheet" href="https://raw.githubusercontent.com/gelvaziosenac/sistemaescolar/main/api/style.css">';
 
-echo '
-<style>
-* {
-    padding: 0;
-    margin: 0;
-    box-sizing: border-box;
-    display: flex;
-    flex-direction: column;        
-}
-
-.background-06 {
-    background: linear-gradient(90deg, #43a4f9, #1ef4ff);
-}
-
-body{
-    height: 85vh; 
-    display: flex;
-    flex-direction: column;
-    justify-content: space-between;
-}
-
-</style>';
+// echo '
+// <style>
+// * {
+//     padding: 0;
+//     margin: 0;
+//     box-sizing: border-box;
+//     display: flex;
+//     flex-direction: column;
+// }
+//
+// .background-06 {
+//     background: linear-gradient(90deg, #43a4f9, #1ef4ff);
+// }
+//
+// body{
+//     height: 85vh;
+//     display: flex;
+//     flex-direction: column;
+//     justify-content: space-between;
+// }
+//
+// </style>';
 
 echo '<body class="background-06">';
 
