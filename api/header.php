eb860309492909a440ab88947b36c38931627a6a
Gelvazio <gelvazio@hotmail.com>
2024-08-19 19:31:01 -0300

    update

--- api/header.php
+++ api/header.php
@@ -1,8 +1,6 @@
 <?php
 echo '<link rel="stylesheet" href="css/style.css">';
 
-// echo '<link rel="stylesheet" href="https://sistemaescolar.vercel.app/api/style.css">';
-
 $sHTML = '
 <body class="background-06">
 <div class="header">
