7416f13b7ce9ac50c885cf6c4fc736119d35efa3
Gelvazio <gelvazio@hotmail.com>
2024-08-19 19:15:13 -0300

    update

--- api/header.php
+++ api/header.php
@@ -1,32 +1,7 @@
 <?php
-// echo '<link rel="stylesheet" href="style.css">';
-//echo '<link rel="stylesheet" href="https://raw.githubusercontent.com/gelvaziosenac/sistemaescolar/main/api/style.css">';
-
-
-echo '<body class="background-06">';
-echo '
-<style>
-    * {
-        padding: 0;
-        margin: 0;
-        box-sizing: border-box;
-        display: flex;
-        flex-direction: column;
-    }
-
-    .background-06 {
-        background: linear-gradient(90deg, #43a4f9, #1ef4ff);
-    }
-
-    body{
-        height: 85vh;
-        display: flex;
-        flex-direction: column;
-        justify-content: space-between;
-    }
-</style>';
-
+echo '<link rel="stylesheet" href="style.css">';
 $sHTML = '
+<body class="background-06">
 <div class="header">
     <ul>
         <li><a href="aluno.php">Alunos</a></li>
